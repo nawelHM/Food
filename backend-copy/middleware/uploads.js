e693d1b711342afba39603e5d53e403985941660
Nawel Hammouda <nawelks.info@gmail.com>
2026-02-05 18:50:52 +0100

    fix cors352

--- backend-copy/middleware/uploads.js
+++ backend-copy/middleware/uploads.js
@@ -1,15 +1,29 @@
+// middleware/uploads.js
 import multer from "multer";
-import { CloudinaryStorage } from "multer-storage-cloudinary";
-import cloudinary from "../config/cloudinary.js";
+import { v2 as cloudinary } from "cloudinary";
+import streamifier from "streamifier";
 
-const storage = new CloudinaryStorage({
-  cloudinary,
-  params: {
-    folder: "foods",
-    allowed_formats: ["jpg", "png", "jpeg", "webp"],
-  },
+cloudinary.config({
+  cloud_name: process.env.CLOUD_NAME,
+  api_key: process.env.CLOUD_API_KEY,
+  api_secret: process.env.CLOUD_API_SECRET,
 });
 
+// Multer memory storage
+const storage = multer.memoryStorage();
 const upload = multer({ storage });
+
+// Upload to Cloudinary
+export const uploadToCloudinary = (fileBuffer, folder) =>
+  new Promise((resolve, reject) => {
+    const stream = cloudinary.uploader.upload_stream(
+      { folder },
+      (error, result) => {
+        if (result) resolve(result);
+        else reject(error);
+      }
+    );
+    streamifier.createReadStream(fileBuffer).pipe(stream);
+  });
 
 export default upload;
